80ffb04afb638db534618e6c1fe9f94569118876
허찬욱 <herkongho@gmail.com>
2024-12-26 14:38:41 +0900

    layout design

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
+      <body >
         <Header />
-        <main>{children}</main>
+        <main className="p-5 max-w-3xl my-0 mx-auto">{children}</main>
         <Footer />
       </body>
     </html>
